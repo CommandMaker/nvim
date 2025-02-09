1f023be5ec191c4c339db1fd62c99a6df18ea622
CommandMaker <command.maker83@gmail.com>
2025-02-09 18:07:23 +0100

    first commit
    add old settings from the previous config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,71 @@
+local key = vim.keymap.set
+
+local silent = { silent = true }
+
+-- Open file explorer
+key('n', '<C-e>', ':Oil<CR>', silent)
+
+-- Close buffer
+key('n', '<S-q>', ':bd<CR>', silent)
+
+-- Open buffer switching
+key('n', 'gb', ':lua require("snipe").open_buffer_menu()<CR>', silent)
+
+-- Navigation in insert mode
+key('i', '<C-h>', '<Left>', silent)
+key('i', '<C-j>', '<Down>', silent)
+key('i', '<C-k>', '<Up>', silent)
+key('i', '<C-l>', '<Right>', silent)
+
+key('c', '<C-h>', '<Left>', silent)
+key('c', '<C-j>', '<Down>', silent)
+key('c', '<C-k>', '<Up>', silent)
+key('c', '<C-l>', '<Right>', silent)
+
+-- File searching
+key('n', '<C-p>', ':FzfLua files<CR>', silent)
+
+-- Open FzfLua
+key('n', '<C-f>', ':FzfLua<CR>', silent)
+
+-- Search through Neovim help
+key('n', '<C-&>', ':Telescope help_tags<CR>', silent)
+
+-- Exit terminal mode with escape (because default mapping is not working on my macOS)
+key('t', '<ESC>', '<C-\\><C-n>', silent)
+
+-- Don't yank on delete char
+key('n', 'x', '"_x', silent)
+key('n', 'X', '"_X', silent)
+key('v', 'x', '"_x', silent)
+key('v', 'X', '"_X', silent)
+
+-- Don't yank on visual paste
+key('v', 'p', '"_dP', silent)
+
+-- Keep visual mode while indenting
+key('v', '<', '<gv', silent)
+key('v', '>', '>gv', silent)
+
+-- Indent the current line in normal mode
+key('n', '<', ':<<CR>', silent)
+key('n', '>', ':><CR>', silent)
+
+-- Move selected line / block of text in visual mode
+key('v', 'K', ':move \'<-2<CR>gv-gv', silent)
+key('v', 'J', ':move \'>+1<CR>gv-gv', silent)
+
+-- Faster move through the current line
+key('i', '<C-^>', '<C-o>^', silent)
+key('i', '<C-$>', '<C-o>$', silent)
+
+-- Resize the current split
+key('n', '<C-o>', ':vert resize +10<CR>', silent)
+key('n', '<C-l>', ':vert resize -10<CR>', silent)
+
+-- LSP keymaps
+key('n', 'gd', ':Telescope lsp_definitions<CR>', silent)
+key('n', 'gz', ':lua vim.diagnostic.open_float()<CR>', silent)
+key('n', 'ga', ':FzfLua lsp_code_actions<CR>', silent)
+key('n', 'gf', ':lua vim.lsp.buf.format()<CR>', silent)
+key('n', 'gr', ':lua vim.lsp.buf.rename()<CR>', silent)
---
+++ lua/config/settings.lua
@@ -0,0 +1,35 @@
+local options = {
+    list           = true,                                        -- Enable the char list
+    showmode       = false,                                       -- Hide mode in the status bar (disable twice render because of lualine)
+    clipboard      = 'unnamed,unnamedplus',                       -- Copy-paste between vim and everything else
+    swapfile       = false,                                       -- Disable the swaps files
+    tabstop        = 4,                                           -- Set tab to 4 spaces
+    softtabstop    = 4,                                           -- Set tab to 4 spaces
+    shiftwidth     = 4,                                           -- Set tab to 4 spaces
+    expandtab      = true,                                        -- Use spaces instead of tabs
+    smartindent    = true,                                        -- Enable smart indenting
+    number         = true,                                        -- Show lines numbers
+    relativenumber = true,                                        -- Show relative lines numbers
+    signcolumn     = 'yes:1',                                     -- Add an extra line column
+    splitright     = true,                                        -- Vertical split will be at right
+    termguicolors  = true,                                        -- Correct terminal colors
+    undofile       = true,                                        -- Set undo to file
+    cursorline     = true,                                        -- Highlight current line
+    scrolloff      = 8,                                           -- Keep space when scrolling
+    completeopt    = 'menu,menuone,popup,noinsert,noselect',      -- Set the omnicomplete options to show the completion menu
+    foldmethod     = 'manual',                                    -- Enable manual creation of folds
+    foldcolumn     = '1'                                          -- Display folds in the line numbers column
+}
+
+vim.opt.listchars:append('lead:·')
+vim.opt.listchars:append('trail:·')
+vim.opt.listchars:remove('space:·')
+vim.opt.listchars:remove('eol')
+
+for i, j in pairs(options) do
+    vim.opt[i] = j
+end
+
+-- Because on my keyboard mapping the @ key is next to w (thanks macOS), I always type :w@ and it creates a file named '@' at the root of my project
+-- So I just remap the :w@ command to :w
+vim.cmd('abbreviate w@ w')
