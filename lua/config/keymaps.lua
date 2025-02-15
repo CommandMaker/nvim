621706bfa221e854ce646a54fb9ae3538d99302b
CommandMaker <command.maker83@gmail.com>
2025-02-15 22:18:58 +0100

    fix: remove old explorer keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,6 @@ local key = vim.keymap.set
 
 local silent = { silent = true }
 
--- Open file explorer
-key('n', '<C-e>', ':Oil<CR>', silent)
-
 -- Close buffer
 key('n', '<S-q>', ':bd<CR>', silent)
 
